0a1f9d0f28a7a7b16c01989611dd3427685b8388
+ <+>
2021-06-20 14:50:42 -0700

    commit tree c9e543351954a282ec1e0e98143244f3c33383b9

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :edit, :update]
+
   def index
+    @artists = Artist.all
   end
 
   def show
+
   end
 
   def new
@@ -20,12 +24,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
+    
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
 
     if @artist.save
@@ -39,6 +41,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
+
     redirect_to artists_path
   end
 
@@ -47,4 +50,9 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist = Artist.create(name: "Gotye")
+
+song = Song.create(title: "Somebody That I Used To Know")
+
+song.artist = artist
